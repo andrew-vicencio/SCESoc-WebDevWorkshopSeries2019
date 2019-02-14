52f58ac1aa5eb83a5d25385019ab0f810c82fac6
Andrew Vicencio <andrew.vicencio@ott.local>
2019-02-14 17:45:54 -0500

    Changed some styling. Changed timeout and added comments in script.

--- workshop 2 - JavaScript/Project/script.js
+++ workshop 2 - JavaScript/Project/script.js
@@ -3,14 +3,14 @@ var choices = document.querySelectorAll('.choice p');
 
 comp_choice = Math.floor(Math.random() * 3);
 
-choices.forEach((elem, i) => {
+choices.forEach((elem, i) => { // same as doing function (elem, i) { ... }
     elem.value = i;
-    elem.addEventListener('click', (e) => {
+    elem.addEventListener('click', (e) => { // same as doing function (event) { ... }
         if (elem.value == 0 && comp_choice == 2 || elem.value > comp_choice){
             message.textContent = 'User wins!';
         } else {
             message.textContent = 'Computer wins!';
         }
-        setTimeout(() => {message.textContent = 'Pick your poison!'}, 1500);
+        setTimeout(() => {message.textContent = 'Pick your poison!'}, 1000);
     });
 });
