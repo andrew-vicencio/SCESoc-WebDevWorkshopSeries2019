8e4edf03ae52fa2167eccff1572f6b3d321c3635
andrew-vicencio <andrew.v1124@gmail.com>
2019-02-14 20:46:03 -0500

    Fixed bug where comp_choice wouldn't randomize after a choice. Added case if both user and computer chose the same choice.

--- workshop 2 - JavaScript/Project/script.js
+++ workshop 2 - JavaScript/Project/script.js
@@ -2,15 +2,21 @@ const message = document.querySelector('#message');
 var choices = document.querySelectorAll('.choice p');
 
 comp_choice = Math.floor(Math.random() * 3);
+options = ['rock', 'paper', 'scissors'];
 
 choices.forEach((elem, i) => { // same as doing function (elem, i) { ... }
     elem.value = i;
     elem.addEventListener('click', (e) => { // same as doing function (event) { ... }
         if (elem.value == 0 && comp_choice == 2 || elem.value > comp_choice){
-            message.textContent = 'User wins!';
+            message.textContent = `Computer chose ${options[comp_choice]}. User wins!`;
+        } else if (elem.value == comp_choice) {
+            message.textContent = `Both user and computer chose ${options[comp_choice]}. Tied!`
         } else {
-            message.textContent = 'Computer wins!';
+            message.textContent = `Computer chose ${options[comp_choice]}. Computer wins!`;
         }
-        setTimeout(() => {message.textContent = 'Pick your poison!'}, 1000);
+        setTimeout(() => {
+            message.textContent = 'Pick your poison!';
+            comp_choice = Math.floor(Math.random() * 3);
+        }, 2000);
     });
 });
